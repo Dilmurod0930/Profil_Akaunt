737b0c6079c0fba8a95587e6367589f7eaf72d16
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-15 14:28:38 +0500

    path yaratildi  akaunt_lst.html  sahifasi  uchun

--- akaunt/urls.py
+++ akaunt/urls.py
@@ -1,6 +1,7 @@
 from  django.urls import path
-from  .views import  main
+from  .views import  main,  akaunt_lst
 
 urlpatterns = [
     path('', main, name='main'),
+    path('akaunt/', akaunt_lst, name='akaunt_list'),
 ]
