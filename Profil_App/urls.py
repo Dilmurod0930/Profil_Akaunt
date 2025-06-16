6e15c0bfd3cc8925a84befa80d5b05cd8360892e
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-16 20:41:18 +0500

    Profil lst Sahifa Tayyor

--- Profil_App/urls.py
+++ Profil_App/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from  .views import  main
+from  .views import  main, profil_lst
 
 urlpatterns = [
     path('', main, name='main'),
+    path('profil_lst/', profil_lst, name='profil_lst'),
 ]
--- Profil_App/views.py
+++ Profil_App/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import  Profil_Model
 
 # Create your views here.
 def  main(request):
     return render(request, 'main.html')
 
+def  profil_lst(request):
+    akaunt = Profil_Model.objects.all()
+    context = {'akaunt': akaunt}
+    return render(request, 'akaunt/akaunt_lst.html', context = context)
