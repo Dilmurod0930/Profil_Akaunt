2ba8dfa20aa89dc6b3b7a49d4a8a7080568df37a
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-16 20:54:37 +0500

    Add  Pagegacha  hammasi  tayyor  malumotlarni  chiqarayabdi  forma ishladi.  Qo

--- Profil_App/urls.py
+++ Profil_App/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from  .views import  main, profil_lst
+from  .views import  main, profil_lst, profil_add
 
 urlpatterns = [
     path('', main, name='main'),
     path('profil_lst/', profil_lst, name='profil_lst'),
+    path('profil_add/', profil_add, name='profil_add'),
 ]
--- Profil_App/views.py
+++ Profil_App/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import Profil_Form
 from .models import  Profil_Model
 
 # Create your views here.
@@ -9,3 +11,11 @@ def  profil_lst(request):
     akaunt = Profil_Model.objects.all()
     context = {'akaunt': akaunt}
     return render(request, 'akaunt/akaunt_lst.html', context = context)
+
+
+def profil_add(request):
+    form = Profil_Form(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('profil_lst')
+    return render(request, 'akaunt/akaunt_add.html', {'form': form})
