883df76ccedf88aad5a94e93e0246a45c5107599
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-18 21:36:31 +0500

    YUklandi.

--- Profil_App/urls.py
+++ Profil_App/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from  .views import  main, profil_lst, profil_add
+from  .views import  main, profil_lst, profil_add, profil_update
 
 urlpatterns = [
     path('', main, name='main'),
     path('profil_lst/', profil_lst, name='profil_lst'),
     path('profil_add/', profil_add, name='profil_add'),
+    path('profil_update/<int:id>', profil_update, name='profil_update'),
 ]
