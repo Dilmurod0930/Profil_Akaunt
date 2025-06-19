159d85cdabc3f8230df5963687fb00284f8f31e4
Dilmurod0930 <dilmurodqurbonov2002@gmail.com>
2025-06-19 07:27:59 +0500

    Delete  sahifasiga  fayl  yaratildi  Sahifaga  bog'landi path lar

--- Profil_App/urls.py
+++ Profil_App/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from  .views import  main, profil_lst, profil_add, profil_update
+from  .views import  main, profil_lst, profil_add, profil_update, profil_del
 
 urlpatterns = [
     path('', main, name='main'),
     path('profil_lst/', profil_lst, name='profil_lst'),
     path('profil_add/', profil_add, name='profil_add'),
-    path('profil_update/<int:id>', profil_update, name='profil_update'),
+    path('profil_update/<int:id>/', profil_update, name='profil_update'),
+    path('profil_del/<int:id>/', profil_del, name='profil_del'),
 ]
